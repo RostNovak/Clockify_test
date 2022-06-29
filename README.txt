3ad99acfd284f272ac15e410ed8a36f69ff0db7b
Rostislav <l.rostislav01@gmail.com>
2022-06-29 15:24:54 +0300

    stdout for all time entries wrote instructions into readme

---
+++ README.txt
@@ -0,0 +1,6 @@
+This proggram is made to get all of your profile`s time entries.
+You have to get your API key from clockify 
+and set it`s value to api_key variable in code (line 4)
+Then we get userID & workspaseID
+Aftwerwards we get list of time entries 
+and going throug all entries we set them in a pandas dataset which we are printing in the end
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 api_key='NWZkMzViMmEtOGEzOS00NTg1LTgxNGItY2UxZTY1YzUzMGFl'
 data = {'x-api-key': api_key}
@@ -8,4 +9,18 @@ r = requests.get('https://api.clockify.me/api/v1/user', headers=data)
 userId = r.json()["id"]
 workspaceId = r.json()["activeWorkspace"]
 r = requests.get(f'https://api.clockify.me/api/v1/workspaces/{workspaceId}/user/{userId}/time-entries', headers=data)
-print(r.content)
+
+df = pd.DataFrame(columns = ['id',"description","start",'end','duration'])
+for i in range(len(r.json())):
+    df.loc[i,'id'] = r.json()[i]['id']
+
+for i in df.index:
+    id = df.loc[i]['id']
+    r = requests.get(f'https://api.clockify.me/api/v1/workspaces/{workspaceId}/time-entries/{id}',
+                     headers=data)
+    df.loc[i, 'description'] = r.json()['description']
+    df.loc[i, 'start'] = r.json()['timeInterval']['start']
+    df.loc[i, 'end'] = r.json()['timeInterval']['end']
+    df.loc[i, 'duration'] = r.json()['timeInterval']['duration']
+pd.set_option('max_columns', None)
+print(df)
